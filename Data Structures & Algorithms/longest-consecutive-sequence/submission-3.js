409c7c1d0d97d440ea54386e5b4aff1ba92cb1a7
Julien Scvt <julien.scouvemont@gmail.com>
2026-06-18 01:45:43 +0200

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let max = nums[0];
+        let min = nums[0]
+        const s = new Set()
+        const finalArray = []
+
+
+        for(const num of nums) {
+            if(num > max) {
+                max = num;
+            }
+            if(num < min) {
+                min = num
+            }
+            s.add(num)
+        }
+
+
+        for(let i = min; i <= max; i++) {
+            finalArray[i - min] = s.has(i)
+        }
+
+        console.log(finalArray)
+
+
+        let maxCount = 0;
+        let currentCount = 0;
+        for(const x of finalArray) {
+            if(x) {
+                currentCount++;
+                if(currentCount > maxCount) {
+                    maxCount = currentCount
+                }
+            }
+            else {
+                if(currentCount > maxCount) {
+                    maxCount = currentCount
+                }
+                currentCount = 0;
+            }
+        }
+        return maxCount
+
+    }
+}
