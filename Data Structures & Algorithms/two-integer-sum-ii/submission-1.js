c7670a7ee5a661f1e28691e85893989cb7e040e2
Julien Scvt <julien.scouvemont@gmail.com>
2026-06-18 13:40:30 +0200

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let i = 0
+        let j = numbers.length - 1
+
+        while(i < j) {
+            if(numbers[j] + numbers[i] > target) {
+                j--
+            }
+            else if(numbers[j] + numbers[i] < target) {
+                i++
+            }
+            else {
+                return [i + 1, j + 1]
+            }
+
+        }
+    }
+}
